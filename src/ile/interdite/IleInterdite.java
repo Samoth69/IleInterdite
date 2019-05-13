9e96dce9ba3103d294949228e9702679d6fab891
violentt <violentt@iut2.univ-grenoble-alpes.fr>
2019-05-13 14:07:26 +0200

    Version 2

--- src/ile/interdite/IleInterdite.java
+++ src/ile/interdite/IleInterdite.java
@@ -17,6 +17,7 @@ public class IleInterdite {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Bonjour (de la part de Monsieur Blanchon)");
+        System.out.println("Nouvelle commande");
     }
     
 }
